f76a9f451df0f2b744d9c26076de05a4f2f2ba31
haidersafi530 <haidersafi2000@gmail.com>
2025-02-27 22:03:00 +0500

    Haider Safi

---
+++ Pass_the_message/script.js
@@ -0,0 +1,5 @@
+const messageInput = document.getElementById('message-input');
+function getMessage(){
+    document.getElementById('message-output').innerHTML = messageInput.value;
+    messageInput.value = "";
+}
