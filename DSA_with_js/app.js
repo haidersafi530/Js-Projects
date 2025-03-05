be6529dc614c9a1c9d308bf12fba9ef56dd11666
haidersafi530 <haidersafi2000@gmail.com>
2025-03-05 16:07:24 +0500

    DSA break 1

---
+++ DSA_with_js/app.js
@@ -0,0 +1,127 @@
+//Haider Safi
+// let a = 10;
+// let b = 20;
+// console.log(a+b);
+
+// let a = 10;
+// let b = "20";
+// console.log(typeof(a+b));
+
+// let a = 10;
+// let b = 20;
+// console.log("sum of 10 and 20 " + (a+b));
+// console.log("1" +1); // Ans 11
+
+//Accept and print the answer;
+
+// let age  = prompt("What is your age");
+// age = Number(age);
+// console.log(typeof age);
+
+// Type Casting 
+// let a = 10;
+// let b = 20;
+// let c = 30;
+// [b,a] = [a,c];
+// console.log(a,b)
+
+// let c = a;  //10
+// b = a; // 10
+// b = c; // 10
+// a = a + b + c;
+// a = a - b;
+
+// console.log(b);
+// console.log(a);
+// console.log(c);
+
+// let a = 12;
+// let b = 20;
+// console.log(Math.floor(a/b));
+
+// let a = 7;
+// let b = 2;
+// console.log(Math.floor(b/a));
+
+// let a = 4567;
+// console.log(a % 10);
+
+//Comparison and Relational Operators 
+//< >  <= >= !=
+// console.log(10 != 10);
+
+//Logical Operators 
+//  && || 
+// console.log(10 > 6 || 15<9);
+
+//Unairy Operators 
+// let i = 11;
+// i = i++ + ++i;
+// console.log(i); //ANS 24
+
+// let a = true;
+// a++;
+// console.log(a);
+
+// let a = 10;
+// console.log(a);
+
+// let a = 10;
+// let b = ++ (a++);
+// console.log(b);
+
+//Math Functions 
+// console.log(Math.round(10.3));//10
+// console.log(Math.ceil(10.1));
+// console.log(Math.floor(10.9));
+// console.log(Math.trunc(18.8));
+// console.log(Math.pow(2,5));
+// console.log(Math.sqrt(128)); 
+// console.log(Math.abs(-15));
+// console.log(Math.max(50,40,20));
+// console.log(Math.min(10,20,50));
+// console.log(Math.trunc(Math.random()*9999));
+
+// let a = 89.47875;
+// console.log(Number(a.toFixed(2)));
+
+//Math Problems 
+//Calculate Area and Perameters
+// let a = 5;
+// let b = 7;
+// console.log(a*b);
+
+// let a = 5;
+// let b = 2;
+// let c = 3;
+// let s = (a+b+c)/2;
+// console.log(s)
+// console.log(Math.sqrt(s * (s-a) * (s-b)));
+
+// var ans = Number(prompt("what is your age"));
+// a = 10;
+// b = 20  ;
+
+// if(a > b){
+//     console.log("Yes")
+// }
+// else{
+//     console.log("No")
+// }
+// var ans = Number(prompt("Enter Your Age")); //Haider
+// if(isNaN(ans)){
+//     console.log("Wrong Input");
+// }
+// else if(ans >= 18){
+//     console.log("You can vote");  
+// }
+// else{
+//     console.log("Your can not vote"); 
+// }
+
+
+var amount =  Number(prompt("what is your final amount ?"));
+if(amount > 0 && amount <= 5000){
+    
+}
+console.log(amount);
