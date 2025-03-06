cc929868f968330a266a3283b78a538381105269
haidersafi530 <haidersafi2000@gmail.com>
2025-03-06 06:52:56 +0500

    Random Numbers updated

--- Find_bins/script.js
+++ Find_bins/script.js
@@ -1,6 +1,20 @@
-function bins(){
-    let prifix = 42;
-    let randomDigits = Math.floor(1000 + Math.random() * 9000);
-    return prifix + randomDigits;
+// function bins(){
+//     let prifix = 42;
+//     let randomDigits = Math.floor(1000 + Math.random() * 9000);
+//     return prifix + randomDigits;
+// }
+// document.write(bins());
+
+
+
+function generateString() {
+    let prefix = "42";  // 
+    let randomDigits = Math.floor(1000 + Math.random() * 9000).toString(); // 4 random digits
+    return prefix + randomDigits;  // 6 
 }
-document.write(bins());
+
+//
+for (let i = 0; i < 1000; i++) {
+    document.write(generateString()+"<br>");    
+}
+
