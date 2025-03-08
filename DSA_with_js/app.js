b22c238da3d139c723385494a284059624e45bf6
haidersafi530 <haidersafi2000@gmail.com>
2025-03-08 16:11:48 +0500

    Dsa Day Mar 8 Time 4:11

--- DSA_with_js/app.js
+++ DSA_with_js/app.js
@@ -155,22 +155,72 @@
 // console.log(amount - Math.floor((display*amount)/100));
 
 
-let unit = Number(prompt("Enter Your Electricity Units"));
-let amount = 0;
+// let unit = Number(prompt("Enter Your Electricity Units"));
+// let amount = 0;
 
-if(unit>400){
-    amount += (unit-400) * 13;
-    unit = 400;
-}
-if(unit>200 && unit <= 400){
-    amount += (unit-200) * 8;
-    unit = 200;
-}
-if(unit>100 && unit <= 200){
-    amount += (unit-100) * 6;
-    unit = 100
-}
-amount += unit*4;
-console.log(amount);
+// if(unit>400){
+//     amount += (unit-400) * 13;
+//     unit = 400;
+// }
+// if(unit>200 && unit <= 400){
+//     amount += (unit-200) * 8;
+//     unit = 200;
+// }
+// if(unit>100 && unit <= 200){
+//     amount += (unit-100) * 6;
+//     unit = 100
+// }
+// amount += unit*4;
+// console.log(amount);
 
+
+// let unit = Number(prompt("Enter your electrycity unit"));
+// let amount = 0;
+// if(unit > 400){
+//     amount += (unit-400) * 13;
+//     unit = 400;
+// }
+// if(unit > 200 && unit <= 400){
+//     amount += (unit-200) * 8;
+//     unit = 200;
+// }
+// if(unit > 100 && unit <= 200){
+//     amount += (unit-100) * 6;
+//     unit = 100;
+// }
+// amount += unit*4;
+// console.log(amount);
+
+
+
+//PKR Deomination
+
+let amount = 5001;
+if(amount>=500){
+    console.log("500 Notes :" + Math.floor(amount/500));
+    amount = amount%500;
+}
+if(amount>=200){
+    console.log("200 Notes :" + Math.floor(amount/200));
+    amount = amount%200;
+}
+if(amount>=100){
+    console.log("100 Nots :" + Math.floor(amount/100));
+    amount = amount%100;
+}
+if(amount>=50){
+    console.log("50 Nots :" + Math.floor(amount/50))
+    amount = amount%50;
+}
+if(amount>=20){
+    console.log("20 Notes :" + Math.floor(amount/20));
+    amount = amount%20;
+}
+if(amount>=10){
+    console.log("10 Notes :" + Math.floor(amount/10));
+    amount = amount%10;
+}
+if(amount === 1){
+    console.log("1 Notes :" + amount);
+}
 
