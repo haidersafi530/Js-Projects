37103b2e646f4b676d3c727fc2abe0dfc427bd1b
haidersafi530 <haidersafi2000@gmail.com>
2025-03-11 23:38:05 +0500

    Dsa Mar 11 Time 11:38 PM

--- DSA_with_js/app.js
+++ DSA_with_js/app.js
@@ -495,19 +495,29 @@
 // }
 // console.log(arr)
 
-let arr = [1, 1, 0, 1, 0, 1, 1, 0, 0];
+// let arr = [1, 1, 0, 1, 0, 1, 1, 0, 0];
 
-let i = 0 , j = 0;
-while(i < arr.length){
-    if(arr[i]===0){
-        let temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-        j++
+// let i = 0 , j = 0;
+// while(i < arr.length){
+//     if(arr[i]===0){
+//         let temp = arr[i];
+//         arr[i] = arr[j];
+//         arr[j] = temp;
+//         j++
+//     }
+//     i++
+// }
+// console.log(arr);
+
+const studentsDatabase = ['Haide', 'Imran', 'Liaqat', 'Adil'];
+const findStudent = (allStudents , StudentName) => {
+    for (let i = 0; i < allStudents.length; i++){
+        if(allStudents[i] === StudentName){
+            console.log(`Found   ${StudentName}`);
+        }
     }
-    i++
 }
-console.log(arr);
+findStudent(studentsDatabase, 'Imran');
 
     
 
