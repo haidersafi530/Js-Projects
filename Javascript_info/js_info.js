ebf2b34375e09558d87d48fa4f1a595b04853d2c
haidersafi530 <haidersafi2000@gmail.com>
2025-03-18 05:25:39 +0500

    Javascript.info Mar18  5:25 AM

---
+++ Javascript_info/js_info.js
@@ -0,0 +1,26 @@
+//Code Structure
+
+// alert("Hello");
+// alert("Hello1");
+
+// alert(3 + 1 + 3);
+// alert(`3 * 3 = : ${3 * 3}`);
+
+// alert("Hello");
+// [1 * 5,2 + 10,3,4,5].forEach(alert);
+// alert("Hello");
+// [1, 2].forEach(alert);
+
+// alert('Hello');
+
+/* An example with two messages.
+This is a multiline comment.
+*/
+// alert('Hello');
+// alert('World');
+
+/* Commenting out the code
+alert('Hello');
+*/
+
+
