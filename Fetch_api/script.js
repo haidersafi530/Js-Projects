d5a39520c6ee885fcefbacb7a9a5845b35697540
haidersafi530 <haidersafi2000@gmail.com>
2025-03-20 18:16:05 +0500

    Fetch Api Mar20  6:15 PM

--- Fetch_api/script.js
+++ Fetch_api/script.js
@@ -1,5 +1,13 @@
+// async function getData() {
+//     setTimeout(function(){
+//         console.log("im inside set time out");
+//     },3000)
+// }
+// let output = getData();
+
 async function getData() {
-    setTimeout(function(){
-        console.log("im inside set time out");
-    },3000)
+    let response = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+    let data = await response.json();
+    console.log(data);
 }
+getData();
