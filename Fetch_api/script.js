78df65d0cfd5da8fc49706a2df57f04e8ac5331c
haidersafi530 <haidersafi2000@gmail.com>
2025-03-20 18:43:38 +0500

    Fetch Api Mar20  6:43 PM

--- Fetch_api/script.js
+++ Fetch_api/script.js
@@ -5,9 +5,27 @@
 // }
 // let output = getData();
 
-async function getData() {
-    let response = await fetch('https://jsonplaceholder.typicode.com/todos/1')
-    let data = await response.json();
-    console.log(data);
-}
-getData();
+// async function getData() {
+//     let response = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+//     let data = await response.json();
+//     console.log(data);
+// }
+// getData();
+
+// async function  getData() {
+//     let response = await fetch('')
+//     let data = await response.json();
+//     console.log(data);
+// }
+// getData();
+fetch("https://pokeapi.co/api/v2/pokemon/pikachu")
+    .then(response => {
+        if(!response.ok){
+            throw new Error("could not fetch resourse");
+        }
+        return response.json();
+    })
+    .then(data => console.log(data.id))
+    .catch(error => console.error(error));
+
+
