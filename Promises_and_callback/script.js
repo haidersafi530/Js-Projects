dda12d31f504aca82d84c3ca81df976335d47c8d
haidersafi530 <haidersafi2000@gmail.com>
2025-03-26 10:42:26 +0500

    Promises and callbacks  Mar 26  10:42 AM

--- Promises_and_callback/script.js
+++ Promises_and_callback/script.js
@@ -1,6 +1,16 @@
-let body = document.getElementsByTagName('body')[0];
+// let body = document.getElementsByTagName('body')[0];
 
 
-setTimeout(() => {
-    console.log("Hi");
-},2000); 
+// setTimeout(() => {
+//     console.log("Hi");
+// },2000); 
+
+//callbacks 
+function sum(a,b){
+    console.log(a+b);
+}
+
+function calculator(a,b,sumCallback) {
+    sumCallback(a,b);
+}
+calculator(1,2,sum);
