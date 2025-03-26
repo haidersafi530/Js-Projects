3895cd5cbecb81a859ded564b8b3c85eac4f4759
haidersafi530 <haidersafi2000@gmail.com>
2025-03-26 10:54:23 +0500

    Promises and callbacks  Mar 26  10:54 AM

--- Promises_and_callback/script.js
+++ Promises_and_callback/script.js
@@ -6,11 +6,39 @@
 // },2000); 
 
 //callbacks 
-function sum(a,b){
-    console.log(a+b);
-}
+// function sum(a,b){
+//     console.log(a+b);
+// }
 
-function calculator(a,b,sumCallback) {
-    sumCallback(a,b);
+// function calculator(a,b,sumCallback) {
+//     sumCallback(a,b);
+// }
+// calculator(1,2, (a,b) => {
+
+// });
+
+// const hello = () => {
+//     console.log("hello");
+// }
+// setTimeout(hello,3000);
+
+//nesting 
+// let age = 18;
+// if(age > 18){
+//     if(age >= 60){
+//         console.log("Senior");
+//     }
+//     else{
+//         console.log("Middle")
+//     }
+// }else{
+//     console.log("child")
+// }
+
+for(let i = 0; i < 5; i++){
+    let str = "";
+    for(let j = 0; j < 5; j++){
+        str = str + j;
+    }
+    console.log(i,str);
 }
-calculator(1,2,sum);
