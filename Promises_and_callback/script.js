3178587463a8fe4220155d79ce30bbd80577c193
haidersafi530 <haidersafi2000@gmail.com>
2025-03-27 14:40:59 +0500

    Promises and callbacks  Mar 27  2:40 PM

--- Promises_and_callback/script.js
+++ Promises_and_callback/script.js
@@ -43,18 +43,116 @@
 //     console.log(i,str);
 // };
 
-function getData(dataId,getNextData) {
-    setTimeout(() => {
-        console.log('data', dataId);
-        if(getNextData){
-            getNextData();
-        }
-    },1000);
+// function getData(dataId,getNextData) {
+//     setTimeout(() => {
+//         console.log('data', dataId);
+//         if(getNextData){
+//             getNextData();
+//         }
+//     },1000);
+// }
+// getData(1, () => {
+//     getData(2, () => {
+//         getData(3, () => {
+//             getData(4);
+//         });
+//     });
+// })
+
+//promise
+
+// let promise = new Promise ((resolve,reject) => {
+//     console.log("im a promise");
+//     reject('error');
+// });
+
+//  let promise = new Promise ((resolve,reject) => {
+//     console.log("Promise");
+//     reject("some error");
+//  });
+ 
+//  function getData(dataId, getNextData){
+//     return new Promise ((resolve,reject) => {
+//        setTimeout(() => {
+//         console.log('Data',dataId);
+//         resolve("success");
+//         if(getNextData){
+//             getNextData()
+//         }
+//        },7000);   
+//     })
+//  };
+
+// let promise = new Promise ((resolve, reject) => {
+//     console.log("im new promise");
+//     reject("some error");
+// }); 
+
+// const getPromise = () => {
+//     return new Promise ((resolve,reject) => {
+//         console.log('im a new promise');
+//         // resolve("success");
+//         reject("network error ");
+//     })
+// };
+// let promise = getPromise();
+// promise.then((res) => {
+//     console.log("promise fullfilled",res); 
+// })
+// promise.catch((err) => {
+//     console.log("rejected",err);
+// });
+
+
+//promise chaining 
+// function getDataFunc() {
+//     return new Promise ((resolve,reject) => {
+//        setTimeout(() => {
+//         console.log('data1');
+//         resolve('success');
+//        },2000);
+//     })   
+// }
+// console.log('fetcing data');
+// let p1 = getDataFunc();
+// p1.then((res) => {
+//     console.log(res);
+// }) 
+// let p2 = getDataFunc();
+// p2.then((res) => {
+//     console.log(res);
+// });
+
+// function getData(dataId, getNextData) {
+//     return new Promise ((resolve, reject) => {
+//         setTimeout(() => {
+//             console.log("data", dataId);
+//             resolve("success");
+//             if(getNextData){
+//                 getNextData();
+//             }
+//         },3000);
+//     });
+// }
+// let p1 = getData(1);
+// p1.then((res) => {
+//     return getData(2);
+// }).then((res) => {
+//     console.log(res);
+// });
+function api() {
+    return new Promise ((resolve,reject) => {
+        setTimeout(() => {
+            console.log('whether data');
+            resolve(200);
+        },2000); 
+    })
 }
-getData(1, () => {
-    getData(2, () => {
-        getData(3, () => {
-            getData(4);
-        });
-    });
-})
+async function getWhetherData() {
+    await api();
+}
+
+
+async function getAllData() {
+    await api()
+}
