e78e8528c948742b018be428cb2aed49563087fa
haidersafi530 <haidersafi2000@gmail.com>
2025-03-27 14:41:47 +0500

    Fetch api wiht project  Mar 27  2:41 PM

--- Fetch_api_with_project/script.js
+++ Fetch_api_with_project/script.js
@@ -3,12 +3,17 @@ const URL = "https://dog.ceo/api/breeds/image/random";
 const dataP = document.querySelector('#data'); // Ensure this is an <img> tag or a valid container
 const getDATA = document.querySelector('#getdata');
 const box = document.querySelector('#box');
+var remove = document.querySelector('#remove');
 
 const getAPI = async () => {
     let response = await fetch(URL);
     let data = await response.json();
-    box.innerHTML = `<img src="${data.message}" alt="Random Dog"/>`;    
+    box.innerHTML = `<img src="${data.message}" alt="Random Dog"/>`;
 };
+
+remove.addEventListener('click', () => {
+    dataP.innerHTML = "";
+})
 
 getDATA.addEventListener('click', getAPI);
 
