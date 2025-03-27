88489a298f18ea4a64325aa193651843fc44f22a
haidersafi530 <haidersafi2000@gmail.com>
2025-03-27 14:43:26 +0500

    Dsa  Mar 27  2:43 PM

--- DSA_with_js/app.js
+++ DSA_with_js/app.js
@@ -1024,12 +1024,11 @@ function getData(dataId) {
     });
 }
 
-async function getAllData() {
+(async function getAllData() {
     await getData(1);
     await getData(2);
     await getData(3);
-    
-}
+})();
 
 
 
