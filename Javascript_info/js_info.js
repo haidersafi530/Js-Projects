ebfb187da31c2c0e3edb9817a375ab2267de8f06
haidersafi530 <haidersafi2000@gmail.com>
2025-03-28 22:40:43 +0500

    JS info from web  Mar 28  10:40 PM

--- Javascript_info/js_info.js
+++ Javascript_info/js_info.js
@@ -23,4 +23,62 @@ This is a multiline comment.
 alert('Hello');
 */
 
+//The modern mode, "use strict"
+// "use strict";   
+// // a = 10;
+// // console.log(a);
+
+// function test(a, b, c){
+//     console.log(a + b + c);
+// }
+// test(10,20,30);
+
+
+//Variables 
+// let message;
+// message = "Hi";
+// alert(message);
+
+// let message = 'Hello';
+// alert(message);
+
+// let user = "Jhon", age = 25, message = "Hello";
+// let user = "Jhon";
+// let age = 24;
+// let message = "Hello";
+
+// let user = "Jhon",
+// age =  25,
+// message = 'Hello';
+
+// var message = 'Hello';
+// let message;
+// message = "Hello";
+// message = "World";
+// alert(message);
+
+// let hello = "Hello";
+// let message;
+// message = 'Hello';
+// alert(hello);
+// alert(message);
+
+// const myBirthday =  '25.09.2003';
+// alert(myBirthday); 
+
+// const COLOR_RED = "#F000";
+// const COLOR_GREEN = "#0F0";
+// const COLOR_BLUE = "#00F";
+// const COLOR_ORANGE = "#FF7F00";
+// let color =  COLOR_ORANGE;
+// alert(color);
+
+
+
+
+
+
+
+
+
 
