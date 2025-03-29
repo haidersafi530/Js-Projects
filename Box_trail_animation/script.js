8e3b252d91f1e8be889db2d5f6e585c789f37805
haidersafi530 <haidersafi2000@gmail.com>
2025-03-29 21:45:33 +0500

    Box Updated w - h  Mar 29  9:45 PM

--- Box_trail_animation/script.js
+++ Box_trail_animation/script.js
@@ -5,6 +5,9 @@ bodyEl.addEventListener('mousemove', (event) => {
     const spanEl = document.createElement('span');
     spanEl.style.left = xPos + 'px';
     spanEl.style.top = yPos + 'px';
+    const size = Math.random()*100;
+    spanEl.style.width = size + 'px';
+    spanEl.style.height = size + 'px                ';
     bodyEl.appendChild(spanEl);
     setTimeout(() => {
         spanEl.remove();
@@ -17,8 +20,9 @@ setTimeout(() => {
 
 setTimeout(() => {
     const h1 = document.createElement("h1");
-    h1.innerHTML = "Haider Safi";
+    h1.innerHTML = "Haider";
     h1.style.color = "#fff";
     h1.style.textAlign = "center";
     bodyEl.appendChild(h1);
 },2000);
+
