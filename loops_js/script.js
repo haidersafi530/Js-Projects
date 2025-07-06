c35e9c913f098a35bf7dfcbccd619338c37c4024
haidersafi530 <haidersafi2000@gmail.com>
2025-07-06 10:41:51 +0500

    loop concept Jul 6 25

---
+++ loops_js/script.js
@@ -0,0 +1,11 @@
+// let p = document.querySelector('#op');
+// for (let i = 1; i<11; i++){
+//     p.innerHTML += i + "<br>";
+// }
+
+//while 
+let i = 1;
+while (i < 30){
+    console.log(i)
+    i++
+}
