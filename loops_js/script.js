d3f2a4fb96da5b76ea6d4004aa8b25528a5f029b
haidersafi530 <haidersafi2000@gmail.com>
2025-07-08 22:32:43 +0500

    loop concept Jul 8 25

--- loops_js/script.js
+++ loops_js/script.js
@@ -3,9 +3,40 @@
 //     p.innerHTML += i + "<br>";
 // }
 
-//while 
-let i = 1;
-while (i < 30){
-    console.log(i)
-    i++
+// //while 
+// let i = 1;
+// while (i < 30){
+//     console.log(i)
+//     i++
+// }
+
+//do while
+
+// let i = 1;
+// do{
+//     console.log(i);
+//     i++
+// }
+// while(i < 20);
+
+// for (let i = 2; i < 200; i++){
+//         console.log(i);
+//         if(i === 32) {
+//             break;
+//         }
+// }
+
+// for (i = 1; i < 200; i++){
+//     if(i === 32){
+//         continue;
+//     }
+//     console.log(i);
+// }
+
+// for (i = 1; i < 10; i++){
+//     console.log(i);
+// }
+
+for (let i = 10;  i > 0; i--){
+    console.log(i);
 }
