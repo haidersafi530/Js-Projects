c6b770a249c0b82db2bdef316440090bbe6be466
haidersafi530 <haidersafi2000@gmail.com>
2025-07-15 10:11:14 +0500

    repeating js event handling

--- dom_manipulation/script.js
+++ dom_manipulation/script.js
@@ -1,4 +1,16 @@
-let li = document.querySelectorAll('ul li:nth-child(2n)');
-li.forEach(function (elem) {
-    elem.classList.add('h1')
-})
+// let li = document.querySelectorAll('ul li:nth-child(2n)');
+// li.forEach(function (elem) {
+//     elem.classList.add('h1')
+// })
+
+let h1 = document.querySelector('h1');
+h1.addEventListener('click', function () {
+    h1.style.color = "red";
+});
+
+let p = document.querySelector('p');
+p.addEventListener('dblclick', function () {
+    p.style.color = 'green';
+    p.style.border = '1px solid black';
+    p.style.padding = '20px';
+});
