08b6cb29672de5f4b94e6ac6b856ce3397639c2d
haidersafi530 <haidersafi2000@gmail.com>
2025-07-24 22:01:48 +0500

    Logic trainiing

--- logic_improving/script.js
+++ logic_improving/script.js
@@ -2,10 +2,32 @@ let inp = document.querySelector('input');
 let p = document.querySelector('p');
 
 inp.addEventListener('input', function () {
-    if(inp.checkValidity()){
-        p.textContent = 'Valid email (HTML5)';
+    let val = inp.value;
+    if(val.length === 0){
+        p.textContent = "enter password";
+        p.style.color = "gray";
+    }
+    else if(val.length < 5) {
+        p.textContent = 'Too weak';
+        p.style.color = 'red';
+
+    }
+    else if(/^[a-zA-Z]+$/.test(val)){
+        p.textContent = 'Weak';
+        p.style.color = "orange";
+
+    }
+    else if(/^[a-zA-Z0-9]+$/.test(val)){
+        p.textContent = "Moderate";
+        p.style.color = "blue";/^[\w!@#\$%\^\&*\)\(+=._-]+$/    
+
+    }
+    else if(/^[\w!@#\$%\^\&*\)\(+=._-]+$/.test(val)){
+        p.textContent = 'Stronge';
+        p.style.color = 'green';
     }
     else{
-        p.textContent = "Invalid email";
+        p.textContent = "Unknown Strength";
+        p.style.color = "black";
     }
-})
+});
