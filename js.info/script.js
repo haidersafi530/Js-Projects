14d1bd4019ab0d6154990fab389c16e52214b217
haidersafi530 <haidersafi2000@gmail.com>
2025-09-29 23:05:12 +0500

    learning from js.info

---
+++ js.info/script.js
@@ -0,0 +1,170 @@
+// alert("Hello");
+// [1,'world'].forEach(alert);
+
+// let message;
+// message = "hello";
+// alert(message);
+
+// let message = "hello";
+// alert(message);
+
+// let user = 'jhon ', age = 25, message = 'hello';
+// alert(user);
+
+// let user = "Jhon";
+// let age = 35;
+// let message = "hello";
+// [user,age,message].forEach(alert);
+
+// let user = 'Haider',
+// age = 25,
+// message = "hello";
+// alert(user+age+message);
+
+// let $ = 1;
+// let _ = 2;
+// alert($ + _);
+
+// let имя = "Hello";   // Russian
+// let 我 = "World";
+
+// num = 5;
+// alert(num);
+
+
+// "use strict";
+// num = 5;
+// alert(num);
+
+// const mybirthday = "18.04.1998";
+// alert(mybirthday);
+
+// let message = 'hello';
+// message = 1264732;
+// alert(message);
+
+// let n = 124;
+// n = 13.642;
+// alert(n);
+
+// alert(1 / 0); // infinty output
+
+// alert( Infinity );
+
+// alert('not a number' / 2);
+
+// alert(NaN + 1);
+// alert( 3 * NaN);
+// alert('not a number' / 2 -1);
+
+// const bigInt = 1234567890123456789012345678901234567890n;
+// alert(bigInt)
+
+
+// let str = 'hello';
+// let str2 = 'single qoute are ok too';
+// let pharse = `can embed another ${str}`;
+// alert(pharse);
+
+// let name = 'jhon';
+// alert(`Hello, ${name} ! `);
+
+// alert(`the result is ${1 + 2}`);
+
+// alert( "the result is ${1 + 2}" );
+
+
+// let nameFieldChecked = true;
+// let ageFieldChecked = false;
+
+
+// let isGrater = 4 > 1;
+// alert(isGrater);
+
+// let age;
+// alert(age);
+
+// let age = 100;
+// age = undefined;
+// alert(age);
+
+// typeof undefined // "undefined"
+
+// typeof 0 //
+
+// alert("hello");
+
+// result = prompt(title, [default]);
+
+// let age = prompt('how old are you', 100);
+
+// alert(`you are ${age} year old!`);
+
+// let test = prompt('test', 'haider');
+
+// alert(`testing ${test}`);
+
+// let test = prompt('Test', '');
+
+
+// let isBoss = confirm('are you boss?');
+
+// alert(isBoss);
+
+
+// let name = prompt('Whats is your name');
+// alert(`my name is ${name}`);
+
+
+// let value = true ;
+// // alert(typeof value);
+
+
+// value = String(value);
+// alert(typeof value); 
+
+// alert("6" / "2");
+
+// let str = "123";
+// // alert(typeof str)
+
+
+// let num = Number(str);
+// alert(typeof num)
+
+// let age = Number("an not a number");
+// alert(age);
+
+// alert(Number("123   "));
+// alert(Number('123z'));
+// alert(Number (true));
+// alert(Number(false));
+
+// alert( Boolean(1) );
+
+// alert( Boolean(1) ); // true
+// alert( Boolean(0) ); // false
+
+// alert( Boolean("hello") ); // true
+// alert( Boolean("") ); // false
+
+// alert( Boolean('0'));
+// alert( Boolean(''));
+
+// let x  = 1 ;
+// x = -x;
+// alert(x);
+
+// let  x = 1 , y = 3;
+// alert(y - x);
+
+// alert(5 % 2);
+
+// alert( 8 % 3);
+
+// alert(4 ** (1/2));
+
+
+
+let s = 'my' + 'string';
+alert(s);
